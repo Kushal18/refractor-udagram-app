446264c106bf9608ea4ff1fc3a060ef7041d9182
“Kushal <kjoshi@marketo.com>
2020-08-22 16:50:12 -0700

    screenshots

--- udagram-api-feed/src/config/config.ts
+++ udagram-api-feed/src/config/config.ts
@@ -4,7 +4,7 @@ export const config = {
   'database': "postgres",
   'host': "udagramkjoshidev.cugn7hnltkzv.us-west-2.rds.amazonaws.com",
   'dialect': 'postgres',
-  'aws_region': "us-west-2a",
+  'aws_region': "us-west-2",
   'aws_profile': "default",
   'aws_media_bucket': "udagaramkjoshidev",
   'url': process.env.URL,
